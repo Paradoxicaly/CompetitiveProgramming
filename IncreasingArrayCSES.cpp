ace8ab0f0015fb011198b09faddf7a82ef141a9d
Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
2025-03-12 18:25:53 -0700

    first commit

---
+++ IncreasingArrayCSES.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n = 0;
+    cin >> n;
+
+    vector <string> words(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+    }
+
+    
+    for (int i = 0; i < words.size(); i++) {
+        
+    }
+}
+    
