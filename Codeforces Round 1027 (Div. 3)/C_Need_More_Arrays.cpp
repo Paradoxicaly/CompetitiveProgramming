276e65c4985f2b1f7e889ebbf12851e1abab3a4e
Derrick K <derrick.kwan.360@gmail.com>
2025-08-02 08:24:12 -0700

    Codeforces Round 1027 (Div. 3)

---
+++ Codeforces Round 1027 (Div. 3)/C_Need_More_Arrays.cpp
@@ -0,0 +1,96 @@
+/*
+Tomato_Cultivator
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) ((int)(x).size())
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rng(i,a,b) for (int i = (a); i < (b); ++i)
+
+const int MOD = 1e9 + 7;
+
+#ifdef Tomato
+
+    return o << "}";
+}
+void bug__(int c, auto ...a) {
+    cerr << "\e[1;" << c << "m";
+    (..., (cerr << a << " "));
+    cerr << "\e[0m" << endl;
+}
+#define bug_(c, x...) bug__(c, __LINE__, "[" + string(#x) + "]", x)
+#define bug(x...) bug_(32, x)
+#else
+#define bug(x...) void(0)
+#endif
+
+template<class... T>
+constexpr auto min(T... a){
+    return std::min({a...});
+}
+
+template<class... T>
+constexpr auto max(T... a){
+    return std::max({a...});
+}
+
+template<class... T>
+void input(T&... a){
+    (cin >> ... >> a);
+}
+
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){
+    cout << a;
+    (cout << ... << (cout << ' ', b));
+    cout << '\n';
+}
+void print() { cout << '\n'; }
+
+ll gcd(ll a, ll b) { 
+    return b ? gcd(b, a % b) : a; 
+}
+
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n;
+    input(n);
+    vi a(n);
+    rep(i, n) cin >> a[i];
+
+    int last = -1e9, cnt = 0;
+    for (int x : a) {
+        if (x >= last + 2) {
+            ++cnt;
+            last = x;
+        }
+    }
+    print(cnt);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    input(T);
+    while (T--) solve();
+    return 0;
+}
