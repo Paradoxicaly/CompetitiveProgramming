6e743738af5da0428b40e8b55b0059af93e4429f
Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
2025-08-03 21:45:12 +0100

    Codeforces Round 1026 (Div. 2)

--- Codeforces Round 1026 (Div. 2)/D_Fewer_Batteries.cpp
+++ Codeforces Round 1026 (Div. 2)/D_Fewer_Batteries.cpp
@@ -6,25 +6,25 @@ Tomato_Cultivator
 using namespace std;
 
 using ll = long long;
-using pii = pair<int,int>;
+using pii = pair<int, int>;
 using vi = vector<int>;
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) ((int)(x).size())
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rng(i,a,b) for (int i = (a); i < (b); ++i)
 
-#define pb      push_back
-#define fi      first
-#define se      second
-#define all(x)  begin(x), end(x)
-#define sz(x)   int((x).size())
-#define rep(i,n)   for(int i=0;i<(n);++i)
-#define rng(i,a,b) for(int i=(a);i<(b);++i)
-
-const int MOD = 1e9+7;
+const int MOD = 1e9 + 7;
 
 #ifdef Tomato
 template <typename T>
-ostream& operator<<(ostream &o, vector<T> vec) {
+ostream& operator<<(ostream &o, vector<T> v) {
     o << "{";
     int f = 0;
-    for (auto &i : vec) o << (f++ ? " " : "") << i;
+    for (auto &&x : v)
+        o << (f++ ? " " : "") << x;
     return o << "}";
 }
 void bug__(int c, auto... a) {
@@ -35,18 +35,13 @@ void bug__(int c, auto... a) {
 #define bug_(c, x...) bug__(c, __LINE__, "[" + string(#x) + "]", x)
 #define bug(x...)    bug_(32, x)
 #else
-#define bug(x...) void(0)
+#define bug(x...)    void(0)
 #endif
 
 template<class... T>
-constexpr auto min(T... a){
-    return std::min({a...});
-}
-
+constexpr auto min(T... a){ return std::min({a...}); }
 template<class... T>
-constexpr auto max(T... a){
-    return std::max({a...});
-}
+constexpr auto max(T... a){ return std::max({a...}); }
 
 template<class... T>
 void input(T&... a){ (cin >> ... >> a); }
@@ -58,76 +53,67 @@ void print(const T& a, const Ts&... b){
     cout << '\n';
 }
 
-void print() { cout << '\n'; }
-
-ll gcd(ll a, ll b) {
-    return b ? gcd(b, a % b) : a;
-}
-
-ll power(ll a, ll b, ll mod = MOD) {
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1) res = res * a % mod;
-        a = a * a % mod;
+ll gcd(ll a, ll b){ return b ? gcd(b, a % b) : a; }
+ll power(ll a, ll b, ll m = MOD){
+    ll r = 1;
+    while (b){
+        if (b & 1) r = (r * a) % m;
+        a = (a * a) % m;
         b >>= 1;
     }
-    return res;
+    return r;
 }
 
-vector<vector<pair<int,int>>> graph;
-
-void solve() {
+void solve(){
     int n, m;
     input(n, m);
-
-    graph.resize(n);
-
     vector<ll> b(n);
-    rep(i, n) input(b[i]);
+    rep(i,n) input(b[i]);
 
-    rep(i, m) {
+    vector<vector<pair<int,int>>> graph(n);
+    rep(i,m){
         int u, v;
         ll w;
         input(u, v, w);
-        graph[u].pb({v, int(w)});
+        --u; --v;
+        graph[u].pb({v, (int)w});
     }
 
-    auto check = [&](ll maxW) {
+    const int INF = int(1e9) + 11;
+    auto check = [&](int maxW){
         vector<ll> best(n, 0);
-        rep(i, n) {
+        rep(i,n){
             if (i > 0 && best[i] == 0) continue;
-            best[i] = min(maxW, best[i] + b[i]);
-            for (auto [to, w] : graph[i]) {
-                if (w <= best[i]) {
-                    best[to] = max(best[to], best[i]);
+            best[i] = min<ll>(maxW, best[i] + b[i]);
+            for (auto &e : graph[i]){
+                if (e.se <= best[i]){
+                    best[e.fi] = max(best[e.fi], best[i]);
                 }
             }
         }
-        return best.back() > 0;
+        return best[n-1] > 0;
     };
 
-    const ll INF = ll(1e9) + 11;
-    if (!check(INF)) {
+    if (!check(INF)){
         print(-1);
         return;
     }
 
-    ll L = 0, R = INF;
-    while (R - L > 1) {
-        ll mid = (L + R) / 2;
-        if (check(mid)) R = mid;
-        else L = mid;
+    int lo = 0, hi = INF;
+    while (hi - lo > 1){
+        int mid = lo + (hi - lo) / 2;
+        if (check(mid)) hi = mid;
+        else lo = mid;
     }
-    print(R);
+    print(hi);
 }
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T = 1;
     input(T);
     while (T--) solve();
-
     return 0;
 }
