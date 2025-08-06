d22a137fd939df817a6164d31c5709701503c770
Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
2025-08-06 21:29:31 +0100

    Update C_3_Hacking_Numbers_Hard_Version.cpp

--- Codeforces Round 1025 (Div. 2)/C_3_Hacking_Numbers_Hard_Version.cpp
+++ Codeforces Round 1025 (Div. 2)/C_3_Hacking_Numbers_Hard_Version.cpp
@@ -6,71 +6,31 @@ Tomato_Cultivator
 using namespace std;
 
 using ll = long long;
-using pii = pair<int, int>;
-using vi = vector<int>;
-#define pb push_back
-#define fi first
-#define se second
-#define all(x) begin(x), end(x)
-#define sz(x) ((int)(x).size())
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-#define rng(i,a,b) for (int i = (a); i < (b); ++i)
-
-const int MOD = 1e9 + 7;
-
-#ifdef Tomato
-template <typename T>
-ostream& operator<<(ostream &o, vector<T> vec) {
-    o << "{"; int f = 0;
-    for (T i : vec) o << (f++ ? " " : "") << i;
-    return o << "}";
-}
-void bug__(int c, auto ...a) {
-    cerr << "\e[1;" << c << "m";
-    (..., (cerr << a << " "));
-    cerr << "\e[0m" << endl;
-}
-#define bug_(c, x...) bug__(c, __LINE__, "[" + string(#x) + "]", x)
-#define bug(x...) bug_(32, x)
-#else
-#define bug(x...) void(0)
-#endif
-
-template<class... T>
-constexpr auto min(T... a){
-    return std::min({a...});
-}
-
-template<class... T>
-constexpr auto max(T... a){
-    return std::max({a...});
-}
-
-template<class... T>
-void input(T&... a){
-    (cin >> ... >> a);
-}
-
-template<class T, class... Ts>
-void print(const T& a, const Ts&... b){
-    cout << a;
-    (cout << ... << (cout << ' ', b));
-    cout << '\n';
-}
-void print() { cout << '\n'; }
 
 void solve() {
     ll n;
-    input(n);
-    if (n <= 9 || n == 81) print(2);
-    else print(3);
+    cin >> n;
+    cout << "mul 999999999" << endl;
+    ll x;
+    cin >> x;  
+
+    cout << "digit" << endl; 
+    cin >> x;
+
+    if (n != 81) {
+        cout << "add " << (n - 81) << endl;
+        cin >> x;
+    }
+
+    cout << "!" << endl;
+    cin >> x;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int T = 1;
-    input(T);
+    int T;
+    cin >> T;
     while (T--) solve();
     return 0;
 }
