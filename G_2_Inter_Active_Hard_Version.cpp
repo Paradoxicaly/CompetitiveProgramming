fa500210d669a7cec7c31414443630760c52afcb
Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
2025-08-09 23:59:09 +0200

    Atto Round 1 (Codeforces Round 1041, Div. 1 + Div. 2)

--- G_2_Inter_Active_Hard_Version.cpp
+++ G_2_Inter_Active_Hard_Version.cpp
@@ -1,187 +1,229 @@
+/*
+Tomato_Cultivator
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 using ll = long long;
-#define all(v) (v).begin(), (v).end()
+using pii = pair<int, int>;
+using vi = vector<int>;
 
-int testcaseCount;
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) ((int)(x).size())
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rng(i,a,b) for (int i = (a); i < (b); ++i)
 
-int request(const vector<int>& arr, int n) {
-    cout << "?";
-    for (int i = 1; i <= n; ++i) cout << ' ' << arr[i];
-    cout << '\n' << flush;
-    int x; cin >> x;
-    return x;
+const int MOD = 1e9 + 7;
+
+#ifdef Tomato
+template <typename T>
+ostream& operator<<(ostream &o, vector<T> vec) {
+    o << "{"; int f = 0;
+    for (T i : vec) o << (f++ ? " " : "") << i;
+    return o << "}";
+}
+void bug__(int c, auto ...a) {
+    cerr << "\e[1;" << c << "m";
+    (..., (cerr << a << " "));
+    cerr << "\e[0m" << endl;
+}
+#define bug_(c, x...) bug__(c, __LINE__, "[" + string(#x) + "]", x)
+#define bug(x...) bug_(32, x)
+#else
+#define bug(x...) void(0)
+#endif
+
+template<class... T>
+void input(T&... a){
+    (cin >> ... >> a);
 }
 
-int determine_bit_width(int x) {
-    return 32 - __builtin_clz(x);
+template<class T, class... Ts>
+void print(const T& a, const Ts&... b){
+    cout << a;
+    (cout << ... << (cout << ' ', b));
+    cout << '\n';
 }
+void print() { cout << '\n'; }
 
-vector<int> encode(int n, int bitLength, vector<int>& permMap) {
-    vector<int> code(n + 1), inv((1 << bitLength));
-    int v = 0;
-    for (int i = 1; i <= n; ++i) {
-        code[i] = (i & 1) ? v++ : ((1 << bitLength) - 1 - v++);
-        permMap[code[i]] = i;
+ll power(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
     }
-    return code;
+    return res;
 }
 
-void xor_deduction(int n, int bitLength, const vector<int>& code, vector<int>& xorMap,
-                   vector<vector<int>>& fwdCheck, vector<vector<int>>& backCheck,
-                   vector<int>& active, vector<int>& inactive, const vector<int>& permMap) {
-    for (int b = 0; b < bitLength; ++b) {
-        vector<int> groupA, groupB;
-        for (int i = 1; i <= n; ++i) {
-            if (code[i] & (1 << b)) groupB.push_back(i);
-            else groupA.push_back(i);
+void solve() {
+    int n;
+    cin >> n;
+    const int k = (n + 1) / 2;
+    const int M = __lg(n - 1) + 1;
+    cout << k + 1 << endl;
+
+    vector<int> p(n + 1);
+
+    auto query = [&](vector<int> &q) -> int {
+        cout << "? ";
+        for (int i = 1; i <= n; i++) cout << q[i] << " \n"[i == n];
+        cout.flush();
+        int res;
+        cin >> res;
+        return res;
+    };
+
+    vector<int> c(n + 1), d(1 << M);
+    auto binary_coding = [&]() {
+        for (int i = 0, cnt = 1; cnt <= n; i++, cnt++) {
+            c[cnt++] = i;
+            if (cnt <= n) c[cnt] = (1 << M) - 1 - i;
+        }
+        for (int i = 1; i <= n; i++) d[c[i]] = i;
+    };
+
+    vector<int> xors(n + 1);
+    vector<vector<int>> res1(M, vector<int>(n + 1)), res2(M, vector<int>(n + 1));
+    vector<int> good, bad;
+
+    auto getXORs = [&]() {
+        for (int bit = 0; bit < M; bit++) {
+            vector<int> A, B;
+            for (int i = 1; i <= n; i++) {
+                if (c[i] & (1 << bit)) B.pb(i);
+                else A.pb(i);
+            }
+
+            int m = A.size(), l = B.size();
+            for (int i = 0; i < m; i++) {
+                vector<int> q(n + 1);
+                for (int j = 0; j < m; j++) q[j + 1] = A[(j + i) % m];
+                for (int j = 0; j < l; j++) q[j + m + 1] = B[j];
+                int res = query(q);
+                res1[bit][q[1]] = res;
+                res2[bit][q[m]] = res;
+            }
+
+            for (int i = 0; i < l; i++) {
+                vector<int> q(n + 1);
+                for (int j = 0; j < l; j++) q[j + 1] = B[(j + i) % l];
+                for (int j = 0; j < m; j++) q[j + l + 1] = A[j];
+                int res = query(q);
+                res1[bit][q[1]] = res;
+                res2[bit][q[l]] = res;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                if (res1[bit][i] != res2[bit][i]) xors[i] ^= (1 << bit);
+            }
         }
 
-        int sa = groupA.size(), sb = groupB.size();
-        for (int i = 0; i < sa; ++i) {
-            vector<int> query(n + 1);
-            for (int j = 0; j < sa; ++j) query[j + 1] = groupA[(j + i) % sa];
-            for (int j = 0; j < sb; ++j) query[j + sa + 1] = groupB[j];
-            int res = request(query, n);
-            fwdCheck[b][query[1]] = res;
-            backCheck[b][query[sa]] = res;
+        for (int i = 1; i <= n; i++) {
+            if (xors[i] != 0) good.pb(i);
+            else bad.pb(i);
         }
+    };
 
-        for (int i = 0; i < sb; ++i) {
-            vector<int> query(n + 1);
-            for (int j = 0; j < sb; ++j) query[j + 1] = groupB[(j + i) % sb];
-            for (int j = 0; j < sa; ++j) query[j + sb + 1] = groupA[j];
-            int res = request(query, n);
-            fwdCheck[b][query[1]] = res;
-            backCheck[b][query[sb]] = res;
+    auto solve3 = [&]() {
+        for (auto pos : good) {
+            if (p[pos]) continue;
+            int x = xors[pos], bit = __lg(x & (-x));
+            vector<int> A, B;
+            for (int i = 1; i <= n; i++) {
+                if (c[i] & (1 << bit)) B.pb(i);
+                else A.pb(i);
+            }
+            if (c[pos] & (1 << bit)) swap(A, B);
+
+            int m = A.size(), l = B.size(), id = 0;
+            for (int i = 0; i < m; i++) if (A[i] == pos) id = i;
+
+            vector<int> base(n + 1);
+            for (int j = 0; j < m; j++) base[j + 1] = A[(j + id) % m];
+            for (int j = 0; j < l; j++) base[j + m + 1] = B[j];
+
+            int L = 2, R = m;
+            while (L < R) {
+                int mid = (L + R) >> 1;
+                vector<int> q(n + 1);
+                for (int i = 1; i < mid; i++) q[i] = base[i + 1];
+                q[mid] = base[1];
+                for (int i = mid + 1; i <= n; i++) q[i] = base[i];
+                int res = query(q);
+                if (res != res1[bit][pos]) R = mid;
+                else L = mid + 1;
+            }
+
+            int s = 0;
+            if (res2[bit][pos] > res1[bit][pos]) p[base[L]] = pos, s = base[L];
+            else p[pos] = base[L], s = pos;
+
+            while (!p[p[s]]) {
+                int X = xors[p[s]];
+                p[p[s]] = d[X ^ c[s]];
+                s = p[s];
+            }
         }
+    };
 
-        for (int i = 1; i <= n; ++i)
-            if (fwdCheck[b][i] != backCheck[b][i])
-                xorMap[i] ^= (1 << b);
-    }
+    auto solve2 = [&]() {
+        for (auto pos : bad) {
+            if (p[pos]) continue;
 
-    for (int i = 1; i <= n; ++i) {
-        if (xorMap[i] == 0) inactive.push_back(i);
-        else active.push_back(i);
-    }
-}
+            auto calc = [&](vector<int> &q) {
+                int val = count(all(p), 0) / 2;
+                vector<int> inv(n + 1);
+                for (int i = 1; i <= n; i++) inv[q[i]] = i;
+                for (int i = 1; i <= n; i++) val += (inv[i] < inv[p[i]]);
+                return val;
+            };
 
-void pairwise_connect(int n, const vector<int>& xorMap, vector<int>& result, const vector<int>& code,
-                      const vector<vector<int>>& fwd, const vector<vector<int>>& back, const vector<int>& permMap) {
-    for (int pos : active) {
-        if (result[pos]) continue;
-        int val = xorMap[pos];
-        int b = __builtin_ctz(val);
+            vector<int> cand, ok;
+            for (int i = 1; i <= n; i++) {
+                if (i != pos && !p[i]) cand.pb(i);
+                if (p[i]) ok.pb(i);
+            }
 
-        vector<int> zero, one;
-        for (int i = 1; i <= n; ++i) {
-            if (code[i] & (1 << b)) one.push_back(i);
-            else zero.push_back(i);
+            while ((int)cand.size() != 1) {
+                vector<int> q = {0}, L, R;
+                int len = ((int)cand.size() + 1) / 2;
+                for (int i = 0; i < len; i++) q.pb(cand[i]), L.pb(cand[i]);
+                for (int i = len; i < k; i++) q.pb(ok[i - len]);
+                q.pb(pos);
+                for (int i = len; i < (int)cand.size(); i++) q.pb(cand[i]), R.pb(cand[i]);
+                for (int i = k - len; i < (int)ok.size(); i++) q.pb(ok[i]);
+
+                int res = query(q) - calc(q);
+                if (res != 0) swap(L, R);
+                cand = L;
+                for (auto x : R) ok.pb(x);
+            }
+
+            p[pos] = cand[0];
+            p[cand[0]] = pos;
         }
-        if (code[pos] & (1 << b)) swap(zero, one);
+    };
 
-        int offset = 0, sizeZero = zero.size();
-        for (int i = 0; i < sizeZero; ++i)
-            if (zero[i] == pos) offset = i;
-
-        vector<int> base(n + 1);
-        for (int i = 0; i < sizeZero; ++i) base[i + 1] = zero[(i + offset) % sizeZero];
-        for (int i = 0; i < one.size(); ++i) base[i + sizeZero + 1] = one[i];
-
-        int L = 2, R = sizeZero;
-        while (L < R) {
-            int M = (L + R) >> 1;
-            vector<int> q(n + 1);
-            for (int i = 1; i < M; ++i) q[i] = base[i + 1];
-            q[M] = base[1];
-            for (int i = M + 1; i <= n; ++i) q[i] = base[i];
-            int res = request(q, n);
-            if (res != fwd[b][pos]) R = M;
-            else L = M + 1;
-        }
-
-        int assigned = 0;
-        if (back[b][pos] > fwd[b][pos]) result[base[L]] = pos, assigned = base[L];
-        else result[pos] = base[L], assigned = pos;
-
-        while (!result[result[assigned]]) {
-            int tmp = xorMap[result[assigned]];
-            result[result[assigned]] = permMap[tmp ^ code[assigned]];
-            assigned = result[assigned];
-        }
-    }
-}
-
-void complete_pairing(int n, const vector<int>& xorMap, vector<int>& result, const vector<int>& permMap) {
-    for (int node : inactive) {
-        if (result[node]) continue;
-
-        vector<int> candidates, paired;
-        for (int i = 1; i <= n; ++i) {
-            if (!result[i] && i != node) candidates.push_back(i);
-            if (result[i]) paired.push_back(i);
-        }
-
-        auto eval = [&](const vector<int>& q) {
-            vector<int> inv(n + 1);
-            int score = count(all(result), 0) / 2;
-            for (int i = 1; i <= n; ++i) inv[q[i]] = i;
-            for (int i = 1; i <= n; ++i) if (result[i]) score += (inv[i] < inv[result[i]]);
-            return score;
-        };
-
-        while (candidates.size() > 1) {
-            vector<int> left, right;
-            int half = (candidates.size() + 1) / 2;
-            vector<int> q = {0};
-            for (int i = 0; i < half; ++i) q.push_back(candidates[i]), left.push_back(candidates[i]);
-            for (int i = half; i < permMap.size() && q.size() < n; ++i) q.push_back(paired[i - half]);
-            q.push_back(node);
-            for (int i = half; i < candidates.size(); ++i) q.push_back(candidates[i]), right.push_back(candidates[i]);
-            for (int i = q.size(); i < n + 1; ++i) q.push_back(1);
-
-            int r = request(q, n) - eval(q);
-            if (r == 0) swap(left, right);
-            candidates = left;
-            paired.insert(paired.end(), all(right));
-        }
-
-        result[node] = candidates[0];
-        result[candidates[0]] = node;
-    }
-}
-
-void handle_test_case() {
-    int n; cin >> n;
-
-    int center = (n + 1) / 2;
-    int bits = determine_bit_width(n - 1);
-    cout << center + 1 << '\n' << flush;
-
-    vector<int> assignment(n + 1), permMap(1 << bits);
-    vector<int> code = encode(n, bits, permMap);
-
-    vector<int> xorMap(n + 1);
-    vector<vector<int>> fwd(bits, vector<int>(n + 1));
-    vector<vector<int>> back(bits, vector<int>(n + 1));
-
-    active.clear(), inactive.clear();
-    xor_deduction(n, bits, code, xorMap, fwd, back, active, inactive, permMap);
-    pairwise_connect(n, xorMap, assignment, code, fwd, back, permMap);
-    complete_pairing(n, xorMap, assignment, permMap);
+    binary_coding();
+    getXORs();
+    solve3();
+    solve2();
 
     cout << "! ";
-    for (int i = 1; i <= n; ++i) cout << assignment[i] << " \n"[i == n];
+    for (int i = 1; i <= n; i++) cout << p[i] << " \n"[i == n];
     cout.flush();
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    cin >> testcaseCount;
-    while (testcaseCount--) handle_test_case();
+    int T = 1;
+    input(T);
+    while (T--) solve();
     return 0;
 }
