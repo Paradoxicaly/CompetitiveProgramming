d40a8df3b12669549c5b635cab76d7791fae810c
Derrick K <91711641+Paradoxicaly@users.noreply.github.com>
2025-08-27 11:40:24 -0700

    Codeforces Round 1045 (Div. 2)

--- Codeforces Round 1045 (Div. 2)/F_Permutation_Oddness.cpp
+++ Codeforces Round 1045 (Div. 2)/F_Permutation_Oddness.cpp
@@ -62,35 +62,47 @@ ll power(ll a, ll b, ll m = MOD){
    return res;
 }
 
+static const int N=800;
+ll jc[2005],invf[2005];
+ll fdp[2][N+5][N+5],resv[N*2+5];
+int a4[4];
+
+inline void addmod(ll& x,ll y){ x+=y; if(x>=MOD) x-=MOD; }
+inline ll C(int n,int k){ if(n<0||k<0||k>n) return 0; return jc[n]*invf[k]%MOD*invf[n-k]%MOD; }
+
+void go(int x,int y,int z){
+    for(int i=0;i<=a4[0]+a4[2];++i) if(fdp[0][x][i]) for(int j=0;j<=a4[1]+a4[3];++j) if(fdp[1][y][j]){
+        resv[(i+j)*2+x+y-1]=(resv[(i+j)*2+x+y-1]+fdp[0][x][i]*fdp[1][y][j]%MOD*z)%MOD;
+    }
+}
+
 void solve(){
-   int n; if(!(cin>>n)) return;
-   vector<int>a(n+3,0),d(n+5,0);
-   d[n+1]=0; d[n+2]=0;
-   for(int i=n;i>=1;--i){
-       if(d[i+1]==d[i+2]) d[i]=d[i+1]+1;
-       else{
-           cout<<"throw "<<i<<'\n'; cout.flush();
-           cin>>d[i];
-           if(d[i]==d[i+1]+1) a[i]=1; else a[i]=2;
+   int T; if(!(cin>>T)) return;
+   jc[0]=1; invf[0]=1;
+   for(int i=1;i<=2000;++i) jc[i]=jc[i-1]*i%MOD;
+   invf[2000]=337096023;
+   for(int i=1999;i>=1;--i) invf[i]=invf[i+1]*(i+1)%MOD;
+   while(T--){
+       int n=0;
+       for(int i=0;i<4;++i){ cin>>a4[i]; n+=a4[i]; }
+       for(int T2=0;T2<=1;++T2){
+           for(int i=0;i<=n+1;++i) for(int j=0;j<=n+1;++j) fdp[T2][i][j]=0;
+           int x=a4[0],y=a4[2];
+           for(int i=2;i<=x+y;++i){
+               int x1=(i-2)/2,y1=(i-2)-x1;
+               ll t1=(C(x-1,y1)*C(y-1,x1)+C(x-1,x1)*C(y-1,y1))%MOD;
+               if(!t1) continue;
+               x1=i-1; y1=x+y-1-x1;
+               for(int j=0;j<=x1;++j) for(int k=0;k<=y1;++k){
+                   addmod(fdp[T2][j+k+1][x1-j], t1*C(x1,j)%MOD*C(y1,k)%MOD);
+               }
+           }
+           swap(a4[0],a4[1]); swap(a4[2],a4[3]);
        }
+       memset(resv,0,sizeof(resv));
+       for(int i=1;i<=n/2+1;++i){ go(i,i,2); go(i,i+1,1); go(i+1,i,1); }
+       for(int i=0;i<=n*2-2;++i){ cout<<(resv[i]%MOD)<<(i==n*2-2?'\n':' '); }
    }
-   for(int i=1;i<=n-1;++i) if(!a[i]){
-       cout<<"swap "<<i<<'\n'; cout.flush();
-       cout<<"throw "<<i+1<<'\n'; cout.flush();
-       int x; cin>>x;
-       if(x==d[i+2]+1) a[i]=1; else a[i]=2;
-   }
-   if(n>=2){
-       cout<<"swap "<<n-1<<'\n'; cout.flush();
-       cout<<"throw "<<n-1<<'\n'; cout.flush();
-       int x; cin>>x;
-       if(x==2) a[n]=1; else a[n]=2;
-   }else{
-       a[1]=1;
-   }
-   cout<<"! ";
-   for(int i=1;i<=n;++i){ cout<<a[i]<<(i==n?'\n':' '); }
-   cout.flush();
 }
 
 int main(){
