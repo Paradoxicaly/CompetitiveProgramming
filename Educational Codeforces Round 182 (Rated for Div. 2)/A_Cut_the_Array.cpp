1d1030bf59a9f47f60d38a44e361786ed0f4f1ef
Derrick K <derrick.kwan.360@gmail.com>
2025-09-15 19:14:50 -0700

    Educational Codeforces Round 182 (Rated for Div. 2)

---
+++ Educational Codeforces Round 182 (Rated for Div. 2)/A_Cut_the_Array.cpp
@@ -0,0 +1,102 @@
+/*
+Tomato_Cultivator
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using vi  = vector<int>;
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) begin(x), end(x)
+#define sz(x) ((int)(x).size())
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define rng(i,a,b) for(int i=(a);i<(b);++i)
+
+
+const int MOD = 1e9+7;
+
+
+#ifdef Tomato
+template<class T>
+ostream& operator<<(ostream &o, const vector<T> &v){
+   o<<"{"; for(int i=0;i<sz(v);i++) o<<(i?" ":"")<<v[i]; return o<<"}";
+}
+template<class A,class B>
+ostream& operator<<(ostream &o,const pair<A,B>&p){ return o<<"("<<p.fi<<","<<p.se<<")"; }
+template<class... A>
+void bug__(int c,A... a){
+   cerr<<"\e[1;"<<c<<"m"; ((cerr<<a<<' '),...); cerr<<"\e[0m\n";
+}
+#define bug_(c,x...) bug__(c,__LINE__,"[" + string(#x) + "]",x)
+#define bug(x...) bug_(32,x)
+#else
+#define bug(x...) ((void)0)
+#endif
+
+
+template<class... T>
+constexpr auto min(T... a){ return std::min({a...}); }
+template<class... T>
+constexpr auto max(T... a){ return std::max({a...}); }
+
+
+template<class... T> void input(T&... a){ (cin>>...>>a); }
+
+
+template<class T,class... Ts>
+void print(const T& a,const Ts&... b){
+   cout<<a; (cout<<...<<(cout<<' ',b)); cout<<'\n';
+}
+void print(){ cout<<'\n'; }
+
+
+template<class T> bool chmin(T& a,const T& b){ if(b<a){a=b; return true;} return false; }
+template<class T> bool chmax(T& a,const T& b){ if(a<b){a=b; return true;} return false; }
+
+
+ll gcdll(ll a,ll b){ return b?gcdll(b,a%b):a; }
+
+
+ll power(ll a, ll b, ll m = MOD){
+   ll res = 1 % m;
+   a %= m;
+   while(b > 0){
+       if(b & 1) res = (res * a) % m;
+       a = (a * a) % m;
+       b >>= 1;
+   }
+   return res;
+}
+
+
+void solve(){
+   int n; cin>>n;
+   vi a(n); rep(i,n) cin>>a[i];
+   vi p(n+1,0);
+   rep(i,n) p[i+1]=(p[i]+a[i])%3;
+   for(int l=1;l<=n-2;++l){
+      for(int r=l+1;r<=n-1;++r){
+         int s1=p[l];
+         int s2=(p[r]-p[l]+3)%3;
+         int s3=(p[n]-p[r]+3)%3;
+         if((s1==s2&&s2==s3)||(s1!=s2&&s2!=s3&&s1!=s3)){
+            print(l,r);
+            return;
+         }
+      }
+   }
+   print(0,0);
+}
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+
+   int T=1;
+   if(cin>>T){
+       while(T--) solve();
+   }
+   return 0;
+}
